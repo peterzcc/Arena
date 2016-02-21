d79f5ddbf4e13cb963acb6b4959ea2df058b6f2f
Peter Z <peter@vimbal.com>
2016-02-22 00:31:45 +0800

    chang dir

--- traker/run_demo_activate.sh
+++ traker/run_demo_activate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-source ~/.bashrc
+#source ~/.bashrc
 
 name=cifar10
 n=5
 s=5
 workerq="all.q@client111,all.q@client112,all.q@client113,all.q@client114,all.q@client115"
 serverq="all.q@client111,all.q@client112,all.q@client113,all.q@client114,all.q@client115"
-wd="/project/dygroup2/czeng/mxnet/example/image-classification/"
+wd="/csproject/dygroup2/czeng/venv/downloads/mxnet/example/image-classification/"
 activate_cmd="./project/dygroup2/czeng/venv/bin/activate"
-script="python /project/dygroup2/czeng/mxnet/example/image-classification/train_cifar10.py --kv-store dist_sync"
+script="python /csproject/dygroup2/czeng/venv/downloads/mxnet/example/image-classification/train_cifar10.py --kv-store dist_sync"
 
 
 #Download CIFAR10 data if no data directory found
@@ -24,4 +24,3 @@ if [ ! -d "cifar10" ]; then
 fi
 
 python dmlc_sge.py --log-file ${name}_n${n}_s${s}.out -wd ${wd} --jobname dmlc-${name}-n${n}-s${s} -workerq ${workerq} -serverq ${serverq} -n ${n} -s ${s} $script
-
