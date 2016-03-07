a9f57531422da99c97602255ec59d561aa019337
sxjscience <xshiab@ust.hk>
2016-03-07 22:58:28 +0800

    Update testing script

--- dqn_run_test.py
+++ dqn_run_test.py
@@ -2,8 +2,24 @@ import mxnet as mx
 import mxnet.ndarray as nd
 import numpy
 import time
+import argparse
+import os
+import re
+import logging
+import sys
 from arena import Critic
 from arena.games import AtariGame
+
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
 
 class DQNOutputOp(mx.operator.NDArrayOp):
     def __init__(self):
@@ -37,6 +53,7 @@ class DQNOutputOp(mx.operator.NDArrayOp):
                                        nd.clip(nd.choose_element_0index(x, action) - reward, -1, 1),
                                        action)
 
+
 def dqn_sym_nature(action_num, output_op):
     net = mx.symbol.Variable('data')
     net = mx.symbol.Convolution(data=net, name='conv1', kernel=(8, 8), stride=(4, 4), num_filter=32)
@@ -53,7 +70,7 @@ def dqn_sym_nature(action_num, output_op):
     return net
 
 
-def collect_holdout_samples(game, num_steps=10000, sample_num=3200):
+def collect_holdout_samples(game, num_steps=3200, sample_num=3200):
     print "Begin Collecting Holdout Samples...",
     game.force_restart()
     game.begin_episode()
@@ -66,16 +83,19 @@ def collect_holdout_samples(game, num_steps=10000, sample_num=3200):
     print "Done!"
     return samples
 
+
 def calculate_avg_q(samples, qnet):
     total_q = 0.0
     for i in xrange(len(samples)):
-        state = nd.array(samples[i:i+1], ctx=q_ctx) / float(255.0)
+        state = nd.array(samples[i:i + 1], ctx=qnet.ctx) / float(255.0)
         total_q += qnet.calc_score(batch_size=1, data=state)[0].asnumpy().max(axis=1).sum()
     avg_q_score = total_q / float(len(samples))
     return avg_q_score
 
+
 def calculate_avg_reward(game, qnet, test_steps=125000, exploartion=0.05):
     game.force_restart()
+    action_num = len(game.action_set)
     total_reward = 0
     steps_left = test_steps
     episode = 0
@@ -98,7 +118,7 @@ def calculate_avg_reward(game, qnet, test_steps=125000, exploartion=0.05):
                     # TODO Profiling the speed of this part!
                     current_state = game.current_state()
                     state = nd.array(current_state.reshape((1,) + current_state.shape),
-                                     ctx=q_ctx) / float(255.0)
+                                     ctx=qnet.ctx) / float(255.0)
                     action = nd.argmax_channel(
                         qnet.calc_score(batch_size=1, data=state)[0]).asscalar()
             else:
@@ -109,42 +129,66 @@ def calculate_avg_reward(game, qnet, test_steps=125000, exploartion=0.05):
         end = time.time()
         steps_left -= game.episode_step
         print 'Episode:%d, FPS:%s, Steps Left:%d, Reward:%d' \
-              %(episode, game.episode_step/(end-start), steps_left, game.episode_reward)
+              % (episode, game.episode_step / (end - start), steps_left, game.episode_reward)
         total_reward += game.episode_reward
-    avg_reward = total_reward/float(episode)
+    avg_reward = total_reward / float(episode)
     return avg_reward
 
-max_start_nullops = 30
-replay_memory_size = 1000000
-holdout_size = 3200
-test_steps = 125000
-exploartion = 0.05
-rows = 84
-cols = 84
-q_ctx = mx.gpu(0)
-minibatch_size = 32
-dir_path = 'dqn-model-norescale-1E-2-wait2'
-model_prefix = 'QNet'
 
-epochs = range(26, 27)
+def main():
+    parser = argparse.ArgumentParser(description='Script to test the trained network on a game.')
+    parser.add_argument('-r', '--rom', required=False, type=str,
+                        default=os.path.join('arena', 'games', 'roms', 'breakout.bin'),
+                        help='Path of the ROM File.')
+    parser.add_argument('-d', '--dir-path', required=False, type=str, default='',
+                        help='Directory path of the model files.')
+    parser.add_argument('-m', '--model_prefix', required=True, type=str, default='QNet',
+                        help='Prefix of the saved model file.')
+    parser.add_argument('-t', '--test-steps', required=False, type=int, default=125000,
+                        help='Test steps.')
+    parser.add_argument('-c', '--ctx', required=False, type=str, default='gpu',
+                        help='Running Context. E.g `-c gpu` or `-c gpu1` or `-c cpu`')
+    parser.add_argument('-e', '--epoch-range', required=False, type=str, default='22',
+                        help='Epochs to run testing. E.g `-e 0,80`, `-e 0,80,2`')
+    parser.add_argument('-v', '--visualization', required=False, type=int, default=0,
+                        help='Visualize the runs.')
+    args, unknown = parser.parse_known_args()
+    max_start_nullops = 30
+    holdout_size = 3200
+    replay_memory_size = 1000000
+    exploartion = 0.05
+    rows = 84
+    cols = 84
+    ctx = re.findall('([a-z]+)(\d*)', args.ctx)
+    ctx = [(device, int(num)) if len(num) >0 else (device, 0) for device, num in ctx]
+    q_ctx = mx.Context(*ctx[0])
+    minibatch_size = 32
+    epoch_range = [int(n) for n in args.epoch_range.split(',')]
+    epochs = range(*epoch_range)
 
-game = AtariGame(resize_mode='scale', resized_rows=rows,
-                 resized_cols=cols, max_null_op=max_start_nullops,
-                 replay_memory_size=replay_memory_size,
-                 death_end_episode=False,
-                 display_screen=False)
-action_num = len(game.action_set)
+    game = AtariGame(resize_mode='scale', resized_rows=rows, replay_start_size=4,
+                     resized_cols=cols, max_null_op=max_start_nullops,
+                     replay_memory_size=replay_memory_size,
+                     death_end_episode=False,
+                     display_screen=args.visualization)
+    if not args.visualization:
+        holdout_samples = collect_holdout_samples(game, sample_num=holdout_size)
+    action_num = len(game.action_set)
+    data_shapes = {'data': (minibatch_size, action_num) + (rows, cols),
+                   'dqn_action': (minibatch_size,), 'dqn_reward': (minibatch_size,)}
+    dqn_output_op = DQNOutputOp()
+    dqn_sym = dqn_sym_nature(action_num, dqn_output_op)
+    qnet = Critic(data_shapes=data_shapes, sym=dqn_sym, name=args.model_prefix, ctx=q_ctx)
 
-holdout_samples = collect_holdout_samples(game, sample_num=holdout_size)
+    for epoch in epochs:
+        qnet.load_params(name=args.model_prefix, dir_path=args.dir_path, epoch=epoch)
+        if not args.visualization:
+            avg_q_score = calculate_avg_q(holdout_samples, qnet)
+            avg_reward = calculate_avg_reward(game, qnet, args.test_steps, exploartion)
+            print "Epoch:%d Avg Reward: %f, Avg Q Score:%f" % (epoch, avg_reward, avg_q_score)
+        else:
+            avg_reward = calculate_avg_reward(game, qnet, args.test_steps, exploartion)
+            print "Epoch:%d Avg Reward: %f" % (epoch, avg_reward)
 
-data_shapes = {'data': (minibatch_size, action_num) + (rows, cols),
-               'dqn_action': (minibatch_size,), 'dqn_reward': (minibatch_size,)}
-dqn_output_op = DQNOutputOp()
-dqn_sym = dqn_sym_nature(action_num, dqn_output_op)
-qnet = Critic(data_shapes=data_shapes, sym=dqn_sym, name='QNet', ctx=q_ctx)
-
-for epoch in epochs:
-    qnet.load_params(name=model_prefix, dir_path=dir_path, epoch=epoch)
-    avg_q_score = calculate_avg_q(holdout_samples, qnet)
-    avg_reward = calculate_avg_reward(game, qnet, test_steps, exploartion)
-    print "Epoch:%d Avg Reward: %f, Avg Q Score:%f" %(epoch, avg_reward, avg_q_score)
+if __name__ == '__main__':
+    main()
