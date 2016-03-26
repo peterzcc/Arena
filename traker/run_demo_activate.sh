5c3ff7415930e93effb5ae178485467b0f06a571
Peter Z <peter@vimbal.com>
2016-03-27 01:29:59 +0800

    configurered run_demo

--- traker/run_demo_activate.sh
+++ traker/run_demo_activate.sh
@@ -3,24 +3,16 @@
 #source ~/.bashrc
 . /project/dygroup2/czeng/venv/bin/activate
 name=cifar10
-n=3
-s=3
-workerq="all.q@client111,all.q@client112,all.q@client113,all.q@client114,all.q@client115,all.q@client108,all.q@client109,all.q@client110"
-serverq="all.q@client111,all.q@client112,all.q@client113,all.q@client114,all.q@client115,all.q@client108,all.q@client109,all.q@client110"
-wd="/csproject/dygroup2/czeng/venv/downloads/mxnet/example/image-classification/"
+n=2
+s=2
+# workerq="all.q@client111,all.q@client112,all.q@client113,all.q@client114,all.q@client115,all.q@client108,all.q@client109,all.q@client110"
+# serverq="all.q@client111,all.q@client112,all.q@client113,all.q@client114,all.q@client115,all.q@client108,all.q@client109,all.q@client110"
+workerq="all.q@client112,all.q@client112,all.q@client113,all.q@client114,all.q@client115,all.q@client108,all.q@client109,all.q@client110"
+serverq="all.q@client112,all.q@client112,all.q@client113,all.q@client114,all.q@client115,all.q@client108,all.q@client109,all.q@client110"
+
+wd="/csproject/dygroup2/czeng/dist_dqn"
 activate_cmd="./project/dygroup2/czeng/venv/bin/activate"
-script="python /csproject/dygroup2/czeng/venv/downloads/mxnet/example/image-classification/train_cifar10.py --kv-store dist_sync"
+script="python /csproject/dygroup2/czeng/dist_dqn/dqn_dist_demo.py  --double-q 1 -c gpu0 -r roms/breakout.bin -eps 0.1"
 
-
-#Download CIFAR10 data if no data directory found
-
-if [ ! -d "cifar10" ]; then
-    mkdir cifar10
-    cd cifar10
-    wget http://webdocs.cs.ualberta.ca/~bx3/data/cifar10.zip
-    unzip -u cifar10.zip
-    mv cifar/* . && rm -rf cifar && rm cifar10.zip
-    cd ..
-fi
 
 python dmlc_sge.py --activate-cmd ${activate_cmd} --log-file ${name}_n${n}_s${s}.out -wd ${wd} --jobname dmlc-${name}-n${n}-s${s} -workerq ${workerq} -serverq ${serverq} -n ${n} -s ${s} $script
