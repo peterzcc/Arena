801c1f3df9015c1b10455b6d00cff99cde5bc8c1
Peter Z <peter@vimbal.com>
2016-03-27 18:34:32 +0800

    update

--- traker/run_demo_activate.sh
+++ traker/run_demo_activate.sh
@@ -3,16 +3,16 @@
 #source ~/.bashrc
 . /project/dygroup2/czeng/venv/bin/activate
 name=breakout
-n=2
-s=2
+n=1
+s=1
 # workerq="*.q@client111,*.q@client112,*.q@client113,*.q@client114,*.q@client115,*.q@client108,*.q@client109,*.q@client110"
 # serverq="*.q@client111,*.q@client112,*.q@client113,*.q@client114,*.q@client115,*.q@client108,*.q@client109,*.q@client110"
-workerq="*.q@client112,*.q@client112,*.q@client113,*.q@client114,*.q@client115,*.q@client108,*.q@client109,*.q@client110"
-serverq="*.q@client112,*.q@client112,*.q@client113,*.q@client114,*.q@client115,*.q@client108,*.q@client109,*.q@client110"
+workerq="*.q@client112,*.q@client114,*.q@client113,*.q@client114,*.q@client115,*.q@client108,*.q@client109,*.q@client110"
+serverq="*.q@client114,*.q@client112,*.q@client113,*.q@client114,*.q@client115,*.q@client108,*.q@client109,*.q@client110"
 
 wd="/csproject/dygroup2/czeng/dist_dqn/"
 activate_cmd="./project/dygroup2/czeng/venv/bin/activate"
-script="python dqn_dist_demo.py  --double-q 1 -c gpu0 -r roms/breakout.bin -eps 0.1"
+script="python kvtest.py  --double-q 1 -c gpu0 -r roms/breakout.bin -eps 0.1 --replay-start-size 500 --kv-type dist_async"
 
 
 python traker/dmlc_sge.py --activate-cmd ${activate_cmd} --log-file ${name}_n${n}_s${s}.out -wd ${wd} --jobname dmlc-${name}-n${n}-s${s} -workerq ${workerq} -serverq ${serverq} -n ${n} -s ${s} $script
