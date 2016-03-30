59b5baa95f713407410d18b99ceb1041fc8b56f1
Peter Z <peter@vimbal.com>
2016-03-30 15:18:12 +0800

    update

--- dqn_async.py
+++ dqn_async.py
@@ -269,10 +269,8 @@ def main():
                 else:
                     action = npy_rng.randint(action_num)
                 actions[g] = action
-            time_before_play = time.time()
             for game,action in zip(games,actions):
                 game.play(action)
-            logging.info("play time: %f"%(time.time()-time_before_play))
             # for ret in parallel_executor.map(play_game, games, actions):
             #     print ret
             # print "finish map"
