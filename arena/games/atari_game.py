d9889736c7aa0bff035b1779e41788d21f6e1667
sxjscience <xshiab@ust.hk>
2016-04-02 17:08:33 +0800

    Give user more hint if the ALE cannot be imported.

--- arena/games/atari_game.py
+++ arena/games/atari_game.py
@@ -1,6 +1,5 @@
 __author__ = 'sxjscience'
 
-from ale_python_interface import ALEInterface
 import mxnet as mx
 import numpy
 import cv2
@@ -19,6 +18,14 @@ _default_rom_path = os.path.join(_dirname, "roms", "breakout.bin")
 
 def ale_load_from_rom(rom_path, display_screen):
     rng = get_numpy_rng()
+    try:
+        from ale_python_interface import ALEInterface
+    except ImportError as e:
+        raise ImportError, 'Unable to import the python package of Arcade Learning Environment. ' \
+                           'ALE may not have been installed correctly. Refer to ' \
+                           '`https://github.com/mgbellemare/Arcade-Learning-Environment` for some' \
+                           'installation guidance'
+
     ale = ALEInterface()
     ale.setInt('random_seed', rng.randint(1000))
     if display_screen:
