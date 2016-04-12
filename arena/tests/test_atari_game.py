c87b90ec9cede8d4a63dda33ee75ac7d993500d9
flyers <lisiyi.zju@gmail.com>
2016-04-12 16:11:57 +0800

    add testing for sample_trajectory

--- arena/tests/test_atari_game.py
+++ arena/tests/test_atari_game.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 replay_start_size = 1000
-game = AtariGame(resize_mode='scale', replay_start_size=replay_start_size, display_screen=False)
+game = AtariGame(resize_mode='scale', replay_memory_size=2000, replay_start_size=replay_start_size, display_screen=False)
 
 game.begin_episode()
 for i in range(2000):
@@ -21,8 +21,8 @@ for i in xrange(10):
     states, actions, rewards, next_states, terminate_flags = new_replay_memory.sample(32)
     gpu_states = nd.array(states, ctx=mx.gpu())/float(255.0)
     gpu_next_states = nd.array(next_states, ctx=mx.gpu())/float(255.0)
-#    plt.imshow(gpu_states.asnumpy()[0,0,:,:] * 255, cmap=cm.Greys_r)
-#    plt.show()
+    # plt.imshow(gpu_states.asnumpy()[0,0,:,:] * 255, cmap=cm.Greys_r)
+    # plt.show()
 ch = raw_input("Press Any Key to Continue")
 game.begin_episode()
 totoal_time_step = 10000
@@ -36,7 +36,8 @@ for i in xrange(totoal_time_step):
     action = numpy.random.randint(0, len(game.action_set))
     reward, terminate_flag = game.play(action)
     if game.episode_terminate:
-        print game.episode_reward, game.episode_step
+        # print game.episode_reward, game.episode_step
+        print '--- begin new episode ---'
         game.begin_episode()
     if game.state_enabled:
         current_state = game.current_state()
@@ -45,11 +46,20 @@ for i in xrange(totoal_time_step):
         states, actions, rewards, next_states, terminate_flags = game.replay_memory.sample(minibatch_size)
         gpu_states = nd.array(states, ctx=mx.gpu())/float(255.0)
         gpu_next_states = nd.array(next_states, ctx=mx.gpu())/float(255.0)
-        #plt.imshow(gpu_states.asnumpy()[0,0,:,:] * 255, cmap=cm.Greys_r)
-        #plt.show()
+        # plt.imshow(gpu_states.asnumpy()[0,0,:,:] * 255, cmap=cm.Greys_r)
+        # plt.show()
         sample_end = time.time()
         sample_total_time += sample_end - sample_start
         sample_total_num += 1
+        state_trajectory, action_trajectory, reward_trajectory, terminate_t = game.replay_memory.sample_trajectory(10)
+        traj_len = state_trajectory.shape[0]
+        print state_trajectory.shape
+        print 'trajectory length is %d, with terminate flag %s' % (traj_len, terminate_t)
+        # for ii in xrange(traj_len):
+        #     plt.imshow(state_trajectory[ii,:,:], cmap=cm.Greys_r)
+        #     plt.show()
+
+
 end = time.time()
 print totoal_time_step/float(end - start)
 print sample_total_num / sample_total_time
