7b9ae5e2c944b75cb1a010d3aabfa11577495f28
sxjscience <xshiab@ust.hk>
2016-04-14 22:44:31 +0800

    Add FFT and IFFT Operators

--- arena/operators.py
+++ arena/operators.py
@@ -1,6 +1,8 @@
 import mxnet as mx
 import mxnet.ndarray as nd
 import numpy
+import pyfftw
+
 # TODO NDArrayOP will cause some troubles see `https://github.com/dmlc/mxnet/issues/1720'
 class DQNOutputOp(mx.operator.NDArrayOp):
     def __init__(self):
@@ -66,3 +68,51 @@ class DQNOutputNpyOp(mx.operator.NumpyOp):
         dx[:] = 0
         dx[numpy.arange(action.shape[0]), action] \
             = numpy.clip(x[numpy.arange(action.shape[0]), action] - reward, -1, 1)
+
+class FFTNpyOp(mx.operator.NumpyOp):
+    def __init__(self):
+        super(FFTNpyOp, self).__init__(need_top_grad=False)
+
+    def list_arguments(self):
+        return ['data']
+
+    def list_outputs(self):
+        return ['real', 'imag']
+
+    def infer_shape(self, in_shape):
+        data_shape = in_shape[0]
+        real_shape = in_shape[0]
+        imag_shape = in_shape[0]
+        return [data_shape], [real_shape, imag_shape]
+
+    def forward(self, in_data, out_data):
+        x = in_data[0]
+        real_part = out_data[0]
+        imag_part = out_data[1]
+        res = pyfftw.interfaces.numpy_fft.fft2(x)
+        real_part[:] = res.real
+        imag_part[:] = res.imag
+
+class IFFTNpyOp(mx.operator.NumpyOp):
+    def __init__(self):
+        super(IFFTNpyOp, self).__init__(need_top_grad=False)
+
+    def list_arguments(self):
+        return ['real', 'imag']
+
+    def list_outputs(self):
+        return ['output']
+
+    def infer_shape(self, in_shape):
+        real_shape = in_shape[0]
+        imag_shape = in_shape[1]
+        output_shape = in_shape[0]
+
+        return [real_shape, imag_shape], output_shape
+
+    def forward(self, in_data, out_data):
+        real_part = in_data[0]
+        imag_part = in_data[1]
+        ifft_res = out_data[0]
+        ifft_res[:] = pyfftw.interfaces.numpy_fft.ifft2(numpy.vectorize(complex)(real_part,
+                                                                                 imag_part))
