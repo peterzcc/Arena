8531509c4b5f05b1c1996c77d3701c15fea7db4c
Peter Z <peter@vimbal.com>
2016-04-15 21:41:20 +0800

    update

--- traker/run_demo_activate.sh
+++ traker/run_demo_activate.sh
@@ -6,7 +6,7 @@ name=breakout
 n=16
 s=5
 
-workerq="*.q@client108,*.q@client109,*.q@client110,*.q@client111,*.q@client112,*.q@client1113,*.q@client114,*.q@client115,*.q@client108,*.q@client109,*.q@client110,*.q@client111,*.q@client112,*.q@client1113,*.q@client114,*.q@client115,"
+workerq="*.q@client108,*.q@client109,*.q@client110,*.q@client111,*.q@client112,*.q@client113,*.q@client114,*.q@client115,*.q@client108,*.q@client109,*.q@client110,*.q@client111,*.q@client112,*.q@client113,*.q@client114,*.q@client115,"
 serverq="*.q@client111,*.q@client112,*.q@client1113,*.q@client114,*.q@client115"
 CTX="gpu1,gpu1,gpu1,gpu1,gpu1,gpu1,gpu1,gpu1,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0,"
 wd="/csproject/dygroup2/czeng/dist_dqn/"
