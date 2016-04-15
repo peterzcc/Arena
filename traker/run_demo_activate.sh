8bc0189ab3c8b69f21f36856d0ea1e743b1a29db
Peter Z <peter@vimbal.com>
2016-04-15 22:23:16 +0800

    update

--- traker/run_demo_activate.sh
+++ traker/run_demo_activate.sh
@@ -7,7 +7,7 @@ n=14
 s=5
 
 workerq="*.q@client111,*.q@client112,*.q@client113,*.q@client114,*.q@client115,*.q@client108,*.q@client109,*.q@client110,*.q@client111,*.q@client113,*.q@client114,*.q@client115"
-serverq="*.q@client111,*.q@client113,*.q@client114,*.q@client115"
+serverq="*.q@client111,*.q@client112,*.q@client113,*.q@client114,*.q@client115"
 CTX="gpu1,gpu1,gpu1,gpu1,gpu1,gpu1,gpu1,gpu1,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0"
 wd="/csproject/dygroup2/czeng/dist_dqn/"
 activate_cmd="./project/dygroup2/czeng/venv/bin/activate"
