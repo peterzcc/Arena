c3f42e19077dab1c24e0bf7469f9a97796a3b3f2
sxjscience <xshiab@ust.hk>
2016-04-17 10:40:57 +0800

    Merge from `vrep` by siyi.

---
+++ arena/iterators.py
@@ -0,0 +1,100 @@
+import numpy
+import cv2
+import re
+import time
+from utils import *
+import matplotlib.pyplot as plt
+import mxnet.ndarray as nd
+
+'''
+A simple data iteration wrapper for tracking sequence data, speed can be optimized.
+
+video_list, a list file for all tracking video data, every line denotes a specific video file path with detailed image jpeg information and roi information
+For example:
+    video_list:
+    /pathtovideo/car1.lst
+    /pathtovideo/car2.lst
+    ...
+    car1.lst
+    /pathtoimg/00001.jpeg x y width height
+    ...
+    Note that the roi format is the left top coordinates with the box width and height, which is consistent with the OTB100 data format.
+
+output_size: desired size after resize
+'''
+
+
+class TrackingIterator(object):
+    def __init__(self, video_list, output_size=(100, 100), resize=True):
+        self.rng = get_numpy_rng()
+        self.img_lists = []
+        self.roi_lists = []
+        with open(video_list) as video_fin:
+            for video_line in video_fin:
+                img_list, roi_list = self._read_image_list(video_line.split()[0])
+                self.img_lists.append(img_list)
+                self.roi_lists.append(roi_list)
+
+        self.output_size = output_size
+        self.video_num = len(self.img_lists)
+        self.resize = resize
+
+    def sample(self, batch_size, length, interval_step=1):
+        video_index = self.rng.randint(0, self.video_num)
+        im_shape = cv2.imread(self.img_lists[video_index][0]).shape
+        if self.resize:
+            data_batch = numpy.zeros((batch_size, length, 3) + self.output_size, dtype=numpy.uint8)
+        else:
+            data_batch = numpy.zeros((batch_size, length, 3) + (im_shape[0], im_shape[1]), dtype=numpy.uint8)
+        roi_batch = numpy.zeros((batch_size, length, 4), dtype=numpy.float32)
+        counter = 0
+        while counter < batch_size:
+            start_index = self.rng.randint(0, len(self.img_lists[video_index]) - (length - 1)*interval_step)
+            for i in xrange(length):
+                im = cv2.imread(self.img_lists[video_index][start_index+i])
+                if self.resize:
+                    im = cv2.resize(im, (self.output_size[1], self.output_size[0]),
+                                interpolation=cv2.INTER_LINEAR)
+                if im.ndim == 2:
+                    im = numpy.tile(im.reshape((1, 3, 3)), (3, 1, 1))
+                else:
+                    im = numpy.rollaxis(im, 2)
+                data_batch[counter, i, :, :, :] = im
+            roi_batch[counter] = numpy.asarray(
+                self.roi_lists[video_index][start_index:start_index+length])
+            counter += 1
+
+        # return data_batch, roi_batch
+        return nd.array(data_batch, ctx=mx.gpu()).asnumpy(), nd.array(roi_batch, ctx=mx.gpu()).asnumpy()
+
+    def _read_image_list(self, file_path):
+        img_list = []
+        roi_list = []
+        with open(file_path) as img_fin:
+            for img_line in img_fin:
+                l = re.split('[ ,]', img_line)
+                if len(l) < 5:
+                    raise IOError
+                img_list.append(l[0])
+                roi_list.append([float(l[1]), float(l[2]), float(l[3]), float(l[4])])
+
+        return img_list, roi_list
+
+track_iter = TrackingIterator('/home/sliay/Documents/OTB100/tmp/video.lst', output_size=(240, 320), resize=True)
+
+start = time.time()
+for i in xrange(20):
+    data_batch, roi_batch = track_iter.sample(batch_size=32, length=10, interval_step=2)
+    print data_batch.shape, roi_batch.shape
+    # for k in xrange(10):
+    #     print roi_batch[0, k]
+    #     cur_img = data_batch[0, k]
+    #     cur_img = numpy.swapaxes(cur_img, 0, 2)
+    #     cur_img = numpy.swapaxes(cur_img, 1, 0)
+    #     print cur_img.shape
+    #     plt.imshow(cur_img)
+    #     plt.show()
+    # print roi_batch[0, :, :]
+
+end = time.time()
+print 'time per minibatch is %f' % ((end - start) / 20.0)
