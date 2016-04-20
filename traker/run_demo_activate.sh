6b93f3299f16eddf890cb9f1b07b9d7a2b3c2662
Peter Z <peter@vimbal.com>
2016-04-20 11:53:24 +0800

    update

--- traker/run_demo_activate.sh
+++ traker/run_demo_activate.sh
@@ -8,10 +8,10 @@ s=4
 
 workerq="*.q@client111,*.q@client112,*.q@client114,*.q@client115,*.q@client108,*.q@client109,*.q@client110,*.q@client111,*.q@client114,*.q@client115,*.q@client108,*.q@client109,*.q@client110"
 serverq="*.q@client111,*.q@client112,*.q@client114,*.q@client115"
-CTX="gpu0,gpu1,gpu0,gpu1,gpu0,gpu1,gpu1,gpu1,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0"
+CTX="gpu0,gpu1,gpu1,gpu0,gpu1,gpu1,gpu1,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0,gpu0"
 wd="/csproject/dygroup2/czeng/dist_dqn/"
 activate_cmd="./project/dygroup2/czeng/venv/bin/activate"
 # script="python easgd_async.py -r roms/breakout.bin -d easgdfixt --replay-start-size 90 --optimizer rmspropnoncentered --lr 0.001 --eps 0.01 --symbol nips --sample-policy recent --rms-decay 0.99 --nactor 16 --single-batch-size 5 --param-update-period 4 --eps-update-period 1000 --kv-type dist_async --easgd-update-period 0.2 --kvstore-update-period 8 --easgd-beta 0.3 --nworker ${n}"
 script="python dqn_async.py -r roms/breakout.bin --replay-start-size 90 --optimizer rmspropnoncentered --lr 0.001 --eps 0.01 --symbol nips --sample-policy recent --rms-decay 0.99 --nactor 16 --single-batch-size 5 --param-update-period 5 --kvstore-update-period 16 --eps-update-period 1000 --kv-type dist_async --nworker ${n}"
 
-python traker/dmlc_sge.py --activate-cmd ${activate_cmd} --log-file ${name}_n${n}_s${s}.out -wd ${wd} --jobname dmlc-${name}-n${n}-s${s} -workerq ${workerq} -serverq ${serverq} -c ${CTX} -n ${n} -s ${s} $script
+python traker/dmlc_sge.py --activate-cmd ${activate_cmd} --log-file ${name}_n${n}_s${s}.out -wd ${wd} --jobname dmlc-${name}-n${n}-s${s} -workerq ${workerq} -serverq ${serverq} -c ${CTX} -n ${n} -s ${s} ${script}
