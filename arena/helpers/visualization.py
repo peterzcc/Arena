390f783a31c7b5d20e5ad82c9060b9105adeb777
sxjscience <xshiab@ust.hk>
2016-04-21 16:09:47 +0800

    Add visualization for tracking result

--- arena/helpers/visualization.py
+++ arena/helpers/visualization.py
@@ -2,6 +2,7 @@ import numpy
 from arena.utils import *
 import mxnet.ndarray as nd
 import matplotlib.pyplot as plt
+import cv2
 import logging
 
 '''
@@ -10,6 +11,8 @@ Description:
     Take an numpy array of shape (n, height, width) or (n, height, width, 3)
     Visualize each (height, width) patch in a grid of size approx. sqrt(n) by sqrt(n)
 '''
+
+
 def visualize_weights(data):
     data = (data - data.min()) / (data.max() - data.min())
 
@@ -20,14 +23,54 @@ def visualize_weights(data):
     data = numpy.pad(data, padding, mode='constant', constant_values=1)
 
     # tile the filters into an image
-    data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
+    data = data.reshape((n, n) + data.shape[1:]).transpose(
+        (0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
     data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
+
     plt.imshow(data)
     plt.axis('off')
     plt.show()
 
+
+'''
+plot the roi bounding box on the image
+im, shape (3, height, width)
+roi, normalized version from [-1,1]
+'''
+
+
+def draw_track_res(im, roi, delay=0):
+    im = im.transpose(1, 2, 0)
+    width = im.shape[1]
+    height = im.shape[0]
+    roi = (roi + 1) / 2 * [width, height, width, height]
+    roi = numpy.uint8(roi)
+    pt1 = (roi[0] - roi[2] / 2, roi[1] - roi[3] / 2)
+    pt2 = (roi[0] + roi[2] / 2, roi[1] + roi[3] / 2)
+    im2 = numpy.zeros(im.shape)
+    im2[:] = im
+    cv2.rectangle(im2, pt1, pt2, (0, 0, 255), 1)
+    cv2.imshow('image', im2[:, :, ::-1] / 255.0)
+    cv2.waitKey(delay)
+
+
+'''
+Simple test for filter visualization and roi drawing
+'''
 if __name__ == '__main__':
+    from arena.iterators import TrackingIterator
     from arena.helpers.pretrained import vgg_m
+
     param = vgg_m()
     conv1 = param['arg:conv1_weight'].asnumpy()
     visualize_weights(conv1.transpose(0, 2, 3, 1))
+
+    track_iter = TrackingIterator('/home/sliay/Documents/OTB100/tmp/video.lst',
+                                  output_size=(240, 320), resize=True)
+
+    data_batch, roi_batch = track_iter.sample(batch_size=32, length=10, interval_step=2)
+    print data_batch.shape, roi_batch.shape
+
+    for i in xrange(data_batch.shape[0]):
+        for j in xrange(data_batch.shape[1]):
+            draw_track_res(data_batch[i, j], roi_batch[i, j], delay=50)
