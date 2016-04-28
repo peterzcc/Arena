afab28a4c1cfe563f4625d2b9d76a1f51fa65e33
sxjscience <xshiab@ust.hk>
2016-04-28 20:14:11 +0800

    Put gaussian_map_fft symbol to the global parameter of the cf class

--- arena/advanced/tracking.py
+++ arena/advanced/tracking.py
@@ -45,9 +45,9 @@ class MeshGridNpyOp(mx.operator.NumpyOp):
 
 
 class GaussianMapGeneratorOp(mx.operator.NumpyOp):
-    def __init__(self, sigma_factor, rows, cols):
+    def __init__(self, sigma_factors, rows, cols):
         super(GaussianMapGeneratorOp, self).__init__(need_top_grad=False)
-        self.sigma_factor = sigma_factor
+        self.sigma_factors = sigma_factors
         self.rows = rows
         self.cols = cols
         x_ind_mat, y_ind_mat = numpy.meshgrid(
@@ -58,26 +58,20 @@ class GaussianMapGeneratorOp(mx.operator.NumpyOp):
                         numpy.square(y_ind_mat).astype(numpy.float32)
 
     def list_arguments(self):
-        return ['attention_size', 'object_size']
+        return []
 
     def list_outputs(self):
         return ['gaussian_map']
 
     def infer_shape(self, in_shape):
-        attention_size_shape = in_shape[0]
-        object_size_shape = in_shape[1]
-        assert object_size_shape == attention_size_shape
-        gaussian_map_shape = (in_shape[0][0], 1, self.rows, self.cols)
-        return [attention_size_shape, object_size_shape], [gaussian_map_shape]
+        gaussian_map_shape = (len(self.sigma_factors), 1, self.rows, self.cols)
+        return [], [gaussian_map_shape]
 
     def forward(self, in_data, out_data):
-        attention_size = in_data[0]
-        object_size = in_data[1]
         gaussian_map = out_data[0]
-        for i in range(attention_size.shape[0]):
-            ratio = numpy.sqrt(numpy.prod(object_size[i] / attention_size[i]).astype(numpy.float32))
-            gaussian_map[i, 0, :, :] = numpy.exp(-self.distance / 2 /
-                                                 numpy.square(ratio * self.sigma_factor))
+        for i, sigma_factor in enumerate(self.sigma_factors):
+            gaussian_map[i, 0, :, :] = numpy.exp(-self.distance / (2 * numpy.square(sigma_factor)))
+
 
 class HannWindowGeneratorOp(mx.operator.NumpyOp):
     def __init__(self, rows, cols):
@@ -100,29 +94,34 @@ class HannWindowGeneratorOp(mx.operator.NumpyOp):
         hanning_map[:] = self.hann_window
 
 
-def gaussian_map_fft(attention_size, object_size, sigma_factor, rows, cols, name=""):
-    gaussian_map_op = GaussianMapGeneratorOp(sigma_factor=sigma_factor, rows=rows, cols=cols)
-    ret = gaussian_map_op(attention_size=attention_size, object_size=object_size, name=name)
-    ret = mx.symbol.FFT2D(data=ret)
-    return ret
-
-
 class CorrelationFilterHandler(object):
     def __init__(self, rows, cols, gaussian_sigma_factor, regularizer, perception_handler,
-                 scale_num=3):
+                 glimpse_handler):
         super(CorrelationFilterHandler, self).__init__()
         self.rows = numpy.int32(rows)
         self.cols = numpy.int32(cols)
         self.out_rows = self.rows
         self.out_cols = (self.cols /2 + 1) * 2
-        self.sigma_factor = gaussian_sigma_factor
+        self.gaussian_sigma_factor = gaussian_sigma_factor
         self.regularizer = regularizer
-        self.scale_num = scale_num
+        self.scale_num = glimpse_handler.scale_num
         self.perception_handler = perception_handler
         hannmap_op = HannWindowGeneratorOp(rows=self.rows, cols=self.cols)
         self.hannmap = hannmap_op()
         self.hannmap = mx.symbol.BroadcastChannel(self.hannmap, dim=0, size=self.scale_num)
         self.hannmap = mx.symbol.BroadcastChannel(self.hannmap, dim=1, size=self.channel_size)
+
+        # Build the sigma_factor list
+        self.sigma_factors = []
+        sigma_factor = self.gaussian_sigma_factor / glimpse_handler.init_scale
+        for i in range(self.scale_num):
+            self.sigma_factors.append(sigma_factor)
+            sigma_factor /= glimpse_handler.scale_mult
+        gaussian_map_op = GaussianMapGeneratorOp(sigma_factors=self.sigma_factors,
+                                                 rows=rows, cols=cols)
+        self.gaussian_map_fft = mx.symbol.FFT2D(data=gaussian_map_op())
+        self.gaussian_map_fft = mx.symbol.BroadcastChannel(self.gaussian_map_fft,
+                                                           dim=1, size=self.channel_size)
 
     @property
     def name(self):
@@ -144,14 +143,7 @@ class CorrelationFilterHandler(object):
         for i in range(glimpse.scale_num):
             object_size_l.append(glimpse.size)
         object_size = mx.symbol.Concat(*object_size_l, num_args=len(object_size_l), dim=0)
-        multiscale_gaussian_map = gaussian_map_fft(attention_size=attention_size,
-                                                   object_size=object_size,
-                                                   sigma_factor=self.sigma_factor,
-                                                   rows=self.rows, cols=self.cols,
-                                                   name=self.name + ":gaussian_map" + postfix)
-        multiscale_gaussian_map = mx.symbol.BroadcastChannel(multiscale_gaussian_map,
-                                                             dim=1, size=self.channel_size)
-        numerator = mx.symbol.ComplexHadamard(multiscale_gaussian_map,
+        numerator = mx.symbol.ComplexHadamard(self.gaussian_map_fft,
                                               mx.symbol.Conjugate(multiscale_feature_fft))
         denominator = mx.symbol.ComplexHadamard(mx.symbol.Conjugate(multiscale_feature_fft),
                                                 multiscale_feature_fft) + \
@@ -171,14 +163,13 @@ class CorrelationFilterHandler(object):
             name=self.name + ":multiscale_feature" + postfix) * self.hannmap
 
         multiscale_feature_fft = mx.symbol.FFT2D(multiscale_feature)
-
         numerator = multiscale_template.numerator
         denominator = mx.symbol.BroadcastChannel(data=multiscale_template.denominator, dim=1,
                                                  size=self.channel_size)
         processed_template = numerator / denominator
         multiscale_scoremap = mx.symbol.IFFT2D(data=mx.symbol.ComplexHadamard(processed_template,
-                                                                 multiscale_feature_fft),
-                                  output_shape=(self.rows, self.cols))
+                                                                              multiscale_feature_fft),
+                                               output_shape=(self.rows, self.cols))
         multiscale_scoremap = mx.symbol.BlockGrad(multiscale_scoremap,
                                                   name=self.name + ':multiscale_scoremap' + postfix)
         return multiscale_scoremap
@@ -227,7 +218,7 @@ class PerceptionHandler(object):
 
 
 class ScoreMapProcessor(object):
-    def __init__(self, dim_in, num_filter=64, scale_num=1):
+    def __init__(self, dim_in, num_filter=4, scale_num=1):
         super(ScoreMapProcessor, self).__init__()
         self.num_filter = num_filter
         self.dim_in = dim_in
