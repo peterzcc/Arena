8a3269cbac2fb8d00380a1e0680875778b084240
sxjscience <xshiab@ust.hk>
2016-05-04 17:22:30 +0800

    Remove backward dependency

--- mxnet-extension/broadcast_channel-inl.h
+++ mxnet-extension/broadcast_channel-inl.h
@@ -128,7 +128,7 @@ namespace mxnet {
         const std::vector<int> &out_grad,
         const std::vector<int> &in_data,
         const std::vector<int> &out_data) const override {
-        return{ out_data[broadcast_channel::kOut] };
+        return{ };
       }
 
       Operator* CreateOperator(Context ctx) const override;
--- mxnet-extension/complex_exchange-inl.h
+++ mxnet-extension/complex_exchange-inl.h
@@ -124,7 +124,7 @@ namespace mxnet {
         const std::vector<int> &out_grad,
         const std::vector<int> &in_data,
         const std::vector<int> &out_data) const override {
-        return{ out_data[complex_exchange_enum::kOut] };
+        return{ };
       }
 
       Operator* CreateOperator(Context ctx) const override;
