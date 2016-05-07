577441604d84ff724ac78a85b0f84c62c4382f8f
sxjscience <xshiab@ust.hk>
2016-05-07 18:55:52 +0800

    Save after 5000 iterations

--- tracking.py
+++ tracking.py
@@ -476,7 +476,7 @@ sample_length = args.sample_length
 BPTT_length = args.BPTT_length
 roll_out_num = args.roll_out
 total_epoch_num = 200
-epoch_iter_num = 10000
+epoch_iter_num = 5000
 baseline_lr = args.baseline_lr
 
 # Score Related Parameters
