1517d5f9d9b47af9a31a4bd56b91b440c2c6a6c6
sxjscience <xshiab@ust.hk>
2016-05-08 20:39:25 +0800

    Fix a bug caused by the previous commit. The take should be done to history_length-1

--- arena/replay_memory.py
+++ arena/replay_memory.py
@@ -111,7 +111,7 @@ class ReplayMemory(object):
             transition_indices = numpy.arange(index, index + self.history_length)
             initial_indices = transition_indices - 1
             end_index = index + self.history_length - 2
-            if numpy.any(self.terminate_flags.take(initial_indices[:self.history_length], mode='wrap')):
+            if numpy.any(self.terminate_flags.take(initial_indices[:(self.history_length-1)], mode='wrap')):
                 # Check if terminates in the middle of the sample!
                 continue
             states[counter] = self.states.take(initial_indices, axis=0, mode='wrap')
