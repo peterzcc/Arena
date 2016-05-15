16899e1cf491db17e775c2b8d2d81714359f4dc3
sxjscience <xshiab@ust.hk>
2016-05-15 12:54:18 +0800

    Revise gradient test for inverse fft2d

--- arena/tests/test_fft_grad.py
+++ arena/tests/test_fft_grad.py
@@ -39,16 +39,19 @@ def foo_irfft(fft_A, fft_B):
     return ret
 
 def grad_foo_irfft(fft_A, fft_B):
+    print 'rfft_A =', fft_A
+    print 'rfft_B =', fft_B
     A = pyfftw.interfaces.numpy_fft.irfft2(fft_A)
     B = pyfftw.interfaces.numpy_fft.irfft2(fft_B)
-    print 'rfft_A =', fft_A
+    print 'A =', A
+    print 'B =', B
     print fft_A[:, 1:]
     print 'fft_A =', pyfftw.interfaces.numpy_fft.fft2(A)
-    print 'rfft_B =', fft_B
     print pyfftw.interfaces.numpy_fft.irfft2(fft_A - fft_A, s=A.shape)
     diff = A - 2*B
     print 'diff =', diff
     grad = pyfftw.interfaces.numpy_fft.rfft2(diff, s=A.shape)
+    print 'grad =', grad
     grad[:] /= A.shape[0] * A.shape[1]
     if 0 == A.shape[1] % 2:
         grad[:, 1:-1] *= 2
@@ -80,15 +83,17 @@ def test_irfft2d_grad():
     in_shape = (5, 5)
     A = numpy.random.rand(*in_shape)*10
     B = numpy.random.rand(*in_shape)*10
-    fft_A = pyfftw.interfaces.numpy_fft.irfft2(A)
-    fft_B = pyfftw.interfaces.numpy_fft.irfft2(B)
-    eps = 1E-6
+    fft_A = pyfftw.interfaces.numpy_fft.rfft2(A)
+    fft_B = pyfftw.interfaces.numpy_fft.rfft2(B)
+    eps = 1E-3
     grad_finite_difference = numpy.zeros(fft_A.shape, dtype=numpy.complex)
     for i in range(fft_A.shape[0]):
         for j in range(fft_A.shape[1]):
-            Z = numpy.zeros(fft_A.shape, dtype=numpy.float32)
-            Z[i, j] = eps
-            grad_finite_difference[i, j] = (foo_irfft(fft_A+Z, fft_B) - foo_irfft(fft_A, fft_B))/eps
+            Z = numpy.zeros(fft_A.shape, dtype=numpy.complex)
+            Z[i, j] = eps + 0j
+            grad_finite_difference[i, j] += (foo_irfft(fft_A+Z, fft_B) - foo_irfft(fft_A, fft_B))/eps
+            Z[i, j] = 0 + 1j*eps
+            grad_finite_difference[i, j] += (foo_irfft(fft_A + Z, fft_B) - foo_irfft(fft_A, fft_B)) / eps * 1j
 
     grad_compute = grad_foo_irfft(fft_A, fft_B)
     print 'grad_finite_difference:', grad_finite_difference
