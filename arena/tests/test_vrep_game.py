d4266d0ae488d0f9e3f37c038f62fdcb45c8b5e4
flyers <lisiyi.zju@gmail.com>
2016-06-06 23:10:03 +0800

    update plot

--- arena/tests/test_vrep_game.py
+++ arena/tests/test_vrep_game.py
@@ -9,7 +9,24 @@ import mxnet.ndarray as nd
 import matplotlib.pyplot as plt
 import logging
 import sys
-from arena.helpers.visualization import draw_track_res
+import cv2
+import argparse
+
+
+def draw_res(im, roi, delay=0):
+    im = im.transpose(1, 2, 0)
+    width = im.shape[0]
+    height = im.shape[1]
+    center = numpy.array([width, height])/2 - roi[0:2] * numpy.array([width, height])
+    center = numpy.int64(center)
+    h = numpy.int64(height * roi[2])
+    im2 = numpy.zeros(im.shape)
+    im2[:] = im
+    cv2.circle(im2, (center[0], center[1]), 4, (0, 0, 255))
+    cv2.line(im2, (1, center[1] - h/2), (width-1, center[1] - h/2), (0, 0, 255))
+    cv2.line(im2, (1, center[1] + h/2), (width-1, center[1] + h/2), (0, 0, 255))
+    cv2.imshow('image', im2[:,:,::-1]/255.0)
+    cv2.waitKey(delay)
 
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
@@ -21,24 +38,33 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 root.addHandler(ch)
 
+parser = argparse.ArgumentParser('script to test vrep game wrapper')
+parser.add_argument('--port', default=19997, type=int, help='remote port on the vrep server side')
+args, unknown = parser.parse_known_args()
+
 rng = get_numpy_rng()
-game = VREPGame(replay_start_size=100)
+itr = 1000000
+game = VREPGame(replay_start_size=100, remote_port=args.port)
 
 #test basic functions of the V-REP simulator
+start = time.time()
+count = 1
 game.begin_episode()
-for i in xrange(500):
+for i in xrange(itr):
     if game.episode_terminate():
+        end = time.time()
+        logging.info('Episode:%d, Reward:%f, fps:%f' % (count, game.episode_reward, game.episode_step / (end-start)))
         game.begin_episode()
-    a = rng.normal(5.335, 0.1, (4, ))
+        start = time.time()
+        count += 1
+    a = rng.normal(0, 0.1, (4, ))
     game.play(a)
     if i%1 == 0:
         # game.print_vrep_data()
-        # plt.imshow(game.image)
-        # plt.show()
         roi = game.target_coordinates
-        im = game.image.transpose(2, 0, 1)
-        roi /= 128
-        draw_track_res(im, roi, delay=10)
+        im = game.image
+        draw_res(im, roi, delay=10)
+
 
 
 # test fps of V-REP
