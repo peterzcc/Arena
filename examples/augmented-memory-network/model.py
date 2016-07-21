1494fae2cb281d5dff1e4c001ca359f4ce854ea9
jennyzhang0215 <jennyzhang0215@gmail.com>
2016-07-21 20:39:48 +0800

    Prepare LRUA

--- examples/augmented-memory-network/model.py
+++ examples/augmented-memory-network/model.py
@@ -4,21 +4,22 @@ import mxnet as mx
 
 class NTMHead(object):
     def __init__(self, memory_size, memory_state_dim, control_state_dim,
-                 init_focus=None, is_write=False, num_shift=3, name="NTMHead"):
+                 init_focus=None, is_write=False, num_shift=3, name="NTMHead", k_smallest=10):
         self.memory_size = memory_size
         self.memory_state_dim = memory_state_dim
         self.control_state_dim = control_state_dim
         self.num_shift = num_shift
         self.name = name
         self.is_write = is_write
+
         self.key_weight = mx.sym.Variable(name=name + ":key_weight")
         self.key_bias = mx.sym.Variable(name=name + ":key_bias")
-        self.gate_weight = mx.sym.Variable(name=name + ":gate_weight")
-        self.gate_bias = mx.sym.Variable(name=name + ":gate_bias")
+        self.alpha_weight = mx.sym.Variable(name=name + ":alpha_weight")
+        self.alpha_bias = mx.sym.Variable(name=name + ":alpha_bias")
         self.beta_weight = mx.sym.Variable(name=name + ":beta_weight")
         self.beta_bias = mx.sym.Variable(name=name + ":beta_bias")
-        self.shift_weight = mx.sym.Variable(name=name + ":shift_weight")
-        self.shift_bias = mx.sym.Variable(name=name + ":shift_bias")
+        #self.shift_weight = mx.sym.Variable(name=name + ":shift_weight")
+        #self.shift_bias = mx.sym.Variable(name=name + ":shift_bias")
         self.gamma_weight = mx.sym.Variable(name=name + ":gamma_weight")
         self.gamma_bias = mx.sym.Variable(name=name + ":gamma_bias")
         self.init_focus = init_focus if init_focus is not None \
@@ -30,8 +31,9 @@ class NTMHead(object):
             self.add_signal_bias = mx.sym.Variable(name=name + ":add_signal_bias")
 
         self.last_step_focus = self.init_focus
+        self.last_weight_focus = self.init_focus # TODO need to change
         self.address_counter = 0
-
+        self.k_smallest = k_smallest
 
     # Controll is FNN
     def controller(self,control_input):
@@ -75,8 +77,8 @@ class NTMHead(object):
             :return: Shape (batch_size, memory_state_dim)
         """
         W_r = self.read_weight(control_input=control_input, memory=memory)
-        # w_t^g = g_t w_t^c + (1 - g_t) w_{t-1}
-        content = mx.sym.sum(mx.sym.broadcast_mul(memory, mx.sym.expand_dims(W_r, axis=2)), axis=1)
+        content = mx.sym.sum(mx.sym.broadcast_mul(memory, mx.sym.expand_dims(W_r, axis=2)),
+                             axis=1)  # TODO Use batch_dot in the future
         return content
 
     def write_weight(self, control_input, memory):
@@ -85,19 +87,32 @@ class NTMHead(object):
             :param memory: Shape (batch_size, memory_size, memory_state_dim)
             :return: Shape (batch_size, memory_size)
         """
-
         # Gamma
         gamma = mx.sym.FullyConnected(data=control_input,
                                       num_hidden=1,
                                       weight=self.gamma_weight,
                                       bias=self.gamma_bias)
         gamma = 1.0 + mx.sym.Activation(data=gamma, act_type='softrelu', name=self.name + ":gamma")
+        # Alpha
+        alpha = mx.sym.FullyConnected(data=control_input,
+                                     num_hidden=1,
+                                     weight=self.alpha_weight,
+                                     bias=self.alpha_bias)
+        alpha = mx.sym.Activation(data=alpha, act_type='sigmoid', name=self.name + ":alpha")
 
         W_r = self.read_weight(control_input=control_input, memory=memory)
-
         W_u = mx.sym.Variable('W_u')
+        W_u_new = mx.sym.broadcast_mul(gamma, W_u) + W_r + self.last_weight_focus # Shape (batch_size, memory_size) TODO batch_dot is not needed here
+        W_lu = mx.sym.k_smallest_flags(W_u_new, k=self.k_smallest) # Shape (batch_size, memory_size)
+        W_w = mx.sym.broadcast_mul(alpha, W_r) + mx.sym.broadcast_mul((1 - alpha), W_lu)
 
-        #W_u =
-        W_lu = mx.sym.Variable('W_lu')
+        self.last_weight_focus = W_w
+        return W_w
 
-        #W_w
+    def write(self, control_input, memory):
+        key = self.controller(control_input)
+        W_w = self.write_weight(control_input=control_input, memory=memory)
+        memory = memory + \
+                 mx.sym.broadcast_mul( mx.sym.expand_dims(W_w, axis=2), mx.sym.expand_dims(key, axis=1) )
+
+
--- examples/augmented-memory-network/test_symbol.py
+++ examples/augmented-memory-network/test_symbol.py
@@ -1,6 +1,296 @@
+
+###################################### Embedding ######################################
+
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+context = mx.symbol.Variable('context')
+embed_weight = mx.sym.Variable("embed_weight")
+
+C = mx.nd.array([[1,2,3,4,5],[1,3,5,7,4],[1,2,5,7,8]],ctx=ctx_haha)
+E = mx.random.uniform(1,10,shape=(10,3),ctx=ctx_haha)
+
+Ain_c = mx.sym.Embedding(data=context, input_dim=10, weight=embed_weight, output_dim=3, name='Ain_c')
+c_exec = Ain_c.bind(ctx=ctx_haha, args={'context' : C, 'embed_weight': E})
+
+c_exec.forward()
+result = c_exec.outputs[0].asnumpy()
+print result
+
+###################################### Reshape ######################################
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+i = mx.symbol.Variable('i')
+ii = mx.nd.array([[1,2,3],[2,3,4],[3,4,5]],ctx=ctx_haha)
+ii.asnumpy()
+re = mx.sym.Reshape(i, shape=(-1, 3, 1))
+re_exec = re.bind(ctx=ctx_haha, args={'i' : ii})
+
+re_exec.forward()
+result = re_exec.outputs[0].asnumpy()
+print result
+
+###################################### dot ######################################
+
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+context = mx.symbol.Variable('context')
+input = mx.symbol.Variable('input')
+
+C = mx.nd.array([[1,2,3],[1,3,5],[1,2,5]],ctx=ctx_haha)
+C.asnumpy()
+I = mx.nd.array([[1],[2],[3]],ctx=ctx_haha)
+I.asnumpy()
+
+MM = mx.sym.dot(context,input)
+
+mm_exec = MM.bind(ctx=ctx_haha, args={'context' : C, 'input': I})
+
+mm_exec.forward()
+result = mm_exec.outputs[0].asnumpy()
+print result
+
+
+###################################### expand_dims ######################################
+
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+key = mx.symbol.Variable('key')
+
+k = mx.nd.array([[1,2,3],[1,3,5]],ctx=ctx_haha)
+k.asnumpy()
+
+MM = mx.sym.expand_dims(key, axis=2)
+
+mm_exec = MM.bind(ctx=ctx_haha, args={'key' : k})
+
+mm_exec.forward()
+result = mm_exec.outputs[0].asnumpy()
+print result.shape
+print result
+
+
 ###################################### broadcast_mul & sum ######################################
 
 import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+probs3dim = mx.symbol.Variable('probs3dim')
+Bin = mx.symbol.Variable('Bin')
+
+P = mx.nd.array([[[1],[2],[3],[4],[5]],[[2],[4],[6],[8],[10]]],ctx=ctx_haha)
+P.asnumpy()
+B = mx.nd.array([[[1,2,3],[1,3,5],[1,2,5],[1,2,5],[1,2,5]],[[1,2,3],[1,3,5],[1,2,5],[1,2,5],[1,2,5]]],ctx=ctx_haha)
+B.asnumpy()
+
+
+MM = mx.sym.broadcast_mul(probs3dim, Bin)
+MM = mx.sym.sum(MM, axis=1)
+
+mm_exec = MM.bind(ctx=ctx_haha, args={'Bin' : B, 'probs3dim': P})
+
+mm_exec.forward()
+result = mm_exec.outputs[0].asnumpy()
+print result
+
+
+###################################### sum ######################################
+
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+probs3dim = mx.symbol.Variable('probs3dim')
+Bin = mx.symbol.Variable('Bin')
+
+#P = mx.nd.array([[1,2,3,4,5],[2,4,6,8,10]],ctx=ctx_haha)
+#P.asnumpy()
+B = mx.nd.array([[[1,2,3],[1,3,5],[1,2,5],[1,2,5],[1,2,5]],[[1,2,3],[1,3,5],[1,2,5],[1,2,5],[1,2,5]]],ctx=ctx_haha)
+B.asnumpy()
+print B.shape
+
+MM = mx.sym.sum(Bin, axis=1)
+
+mm_exec = MM.bind(ctx=ctx_haha, args={'Bin' : B})
+
+mm_exec.forward()
+result = mm_exec.outputs[0].asnumpy()
+print result.shape
+print result
+
+
+###################################### batch_dot ######################################
+
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+probs3dim = mx.symbol.Variable('probs3dim')
+Bin = mx.symbol.Variable('Bin')
+
+P = mx.nd.array([[[1],[2],[3],[4],[5]],[[2],[4],[6],[8],[10]]],ctx=ctx_haha) #2*5*1
+P.asnumpy()
+B = mx.nd.array([[[1,2,3,4,5],[1,3,5,7,9],[1,3,5,7,9]],[[1,2,3,4,5],[1,3,5,7,9],[1,3,5,7,9]]],ctx=ctx_haha) #2*3*5
+B.asnumpy()
+
+
+MM = mx.sym.batch_dot(Bin, probs3dim)
+
+mm_exec = MM.bind(ctx=ctx_haha, args={'Bin' : B, 'probs3dim': P})
+
+mm_exec.forward()
+result = mm_exec.outputs[0].asnumpy()
+print result
+
+
+###################################### - ######################################
+
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+weight = mx.symbol.Variable('weight')
+
+k = mx.nd.array([[1,2,3],[1,3,5]],ctx=ctx_haha)
+k.asnumpy()
+
+MM = 1-weight
+
+mm_exec = MM.bind(ctx=ctx_haha, args={'weight' : k})
+
+mm_exec.forward()
+result = mm_exec.outputs[0].asnumpy()
+print result
+
+###################################### broadcast_mul & sum ######################################
+
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+M = mx.symbol.Variable('M')
+k = mx.symbol.Variable('k')
+w = mx.symbol.Variable('w')
+memory = mx.nd.array([[[1,2,3],[1,3,5],[1,2,5],[1,2,5],[1,2,5]],[[1,2,3],[1,3,5],[1,2,5],[1,2,5],[1,2,5]]],ctx=ctx_haha)
+memory.asnumpy()
+weight = mx.nd.array([[1,2,3,4,5],[2,4,6,8,10]],ctx=ctx_haha)
+weight.asnumpy()
+key = mx.nd.array([[1,1,1],[2,2,2]],ctx=ctx_haha)
+key.asnumpy()
+
+M = mx.sym.broadcast_mul(mx.sym.expand_dims(w, axis=2), mx.sym.expand_dims(k, axis=1) )
+
+mm_exec = M.bind(ctx=ctx_haha, args={'w' : weight, 'k': key})
+
+mm_exec.forward()
+result = mm_exec.outputs[0].asnumpy()
+print result
+
+###################################### SoftmaxActivation ######################################
+
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+Aout = mx.symbol.Variable('Aout')
+
+a = mx.nd.array([[ 14.,  22.,  20.],[ 28.,  44.,  40.]],ctx=ctx_haha)
+a.asnumpy()
+
+softmax = mx.sym.SoftmaxActivation(Aout)
+
+s_exec = softmax.bind(ctx=ctx_haha, args={'Aout' : a})
+
+s_exec.forward()
+result = s_exec.outputs[0].asnumpy()
+print result
+
+###################################### SoftmaxOutput ######################################
+
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+Aout = mx.symbol.Variable('Aout')
+label = mx.symbol.Variable('label')
+
+a = mx.nd.array([[ 14.,  22.,  20.],[ 28.,  44.,  40.]],ctx=ctx_haha)
+a.asnumpy()
+p = mx.nd.array([1,2],ctx=ctx_haha)
+p.asnumpy()
+
+softmax = mx.sym.SoftmaxOutput(data = Aout,label = label)
+
+s_exec = softmax.bind(ctx=ctx_haha, args={'Aout' : a, 'label': p})
+
+s_exec.forward()
+result = s_exec.outputs[0].asnumpy()
+print result
+
+###################################### slice_axis ######################################
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+input = mx.symbol.Variable('input')
+
+D = mx.nd.array([[1,2,3],[1,3,5]],ctx=ctx_haha)
+D.asnumpy()
+
+F = mx.symbol.slice_axis(input, axis=1, begin=2, end=3)
+
+
+F_exec = F.bind(ctx=ctx_haha, args={'input': D})
+
+F_exec.forward()
+result = F_exec.outputs[0].asnumpy()
+print result
+
+
+###################################### Concat ######################################
+import mxnet as mx
+ctx_haha = mx.gpu(0)
+
+left = mx.symbol.Variable('left')
+right = mx.symbol.Variable('right')
+
+l = mx.nd.array([[1,2,3],[1,3,5]], ctx=ctx_haha)
+l.asnumpy()
+r = mx.nd.array([[1,2,3],[1,3,5]], ctx=ctx_haha)
+r.asnumpy()
+
+cat = mx.sym.Concat(left, right, dim=1)
+
+c_exec = cat.bind(ctx=ctx_haha, args={'left': l, 'right':r})
+
+c_exec.forward()
+result = c_exec.outputs[0].asnumpy()
+print result
+
+
+###################################### Fill ######################################
+import mxnet as mx
+import numpy as np
+
+x = mx.nd.zeros((2, 3), dtype=np.float32)
+x.asnumpy()
+x[:] = 1.0
+x.asnumpy()
+
+
+###################################### Compute Cross Entropy using numpy ######################################
+
+import numpy as np
+
+a = np.array([[1,1],[0,1]])
+b = np.array([[0.9,0.1],[0.5,0.5]])
+
+-a*np.log(b)
+np.sum(-a*np.log(b),axis= 0)
+
+
+
+
+###################################### broadcast_mul & sum ######################################
+
+import mxnet as mx
+ctx_haha = mx.gpu(0)
 
 probs3dim = mx.symbol.Variable('probs3dim')
 Bin = mx.symbol.Variable('Bin')
