6895f9b6982a800cd42cfe6e23cc309060504ce0
sxjscience <xshiab@ust.hk>
2016-08-03 14:20:05 +0800

    Fix python2/python3 compatibility

--- examples/neural-turing-machine/copy_task.py
+++ examples/neural-turing-machine/copy_task.py
@@ -1,3 +1,7 @@
+# coding: utf-8
+from __future__ import absolute_import, division, print_function
+from builtins import input
+
 from arena import Base
 from arena.helpers.visualization import *
 from arena.ops import LSTM, NTM
