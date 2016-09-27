3715e955d0febef2e1c7c604e19b4eee6c009e5b
sxjscience <xshiab@ust.hk>
2016-09-27 16:26:18 +0800

    Learn initial memory for the NTM example

--- examples/neural-turing-machine/copy_task.py
+++ examples/neural-turing-machine/copy_task.py
@@ -4,7 +4,7 @@ from builtins import input
 
 from arena import Base
 from arena.helpers.visualization import *
-from arena.ops import RNN, NTM
+from arena.ops import *
 from arena.utils import *
 
 root = logging.getLogger()
@@ -59,19 +59,50 @@ def sym_gen(seqlen):
     data = mx.sym.SliceChannel(data, num_outputs=data_seqlen, axis=0, squeeze_axis=False) # (batch_size, data_dim) * seqlen
     # Initialize Memory
     init_memory = mx.sym.Variable('init_memory')
+    init_read_content = mx.sym.Variable('init_read_content')
     init_read_focus = mx.sym.Variable('NTM->read_head:init_focus')
     init_write_focus = mx.sym.Variable('NTM->write_head:init_focus')
+    init_h = [mx.sym.Variable('controller->layer0:init_h')]
+    init_c = [mx.sym.Variable('controller->layer0:init_c')]
+
+    init_memory = mx.sym.broadcast_to(mx.sym.expand_dims(mx.sym.Activation(init_memory, act_type="tanh"),
+                                                         axis=0),
+                                      shape=(batch_size, memory_size, memory_state_dim))
+    # init_memory = mx.sym.Custom(init_memory, op_type="Identity")
+    init_read_content = mx.sym.broadcast_to(mx.sym.expand_dims(mx.sym.Activation(init_read_content,
+                                                                                 act_type="tanh"),
+                                                               axis=0),
+                                            shape=(batch_size, num_reads, memory_state_dim))
+    init_read_focus = mx.sym.SoftmaxActivation(init_read_focus)
+    init_read_focus = mx.sym.broadcast_to(mx.sym.expand_dims(init_read_focus, axis=0),
+                                          shape=(batch_size, num_reads, memory_size))
+    init_write_focus = mx.sym.SoftmaxActivation(init_write_focus)
+    init_write_focus = mx.sym.broadcast_to(mx.sym.expand_dims(init_write_focus, axis=0),
+                                           shape=(batch_size, num_writes, memory_size))
+    init_h = [mx.sym.broadcast_to(mx.sym.expand_dims(mx.sym.Activation(ele, act_type="tanh"),
+                                                     axis=0),
+                                  shape=(batch_size, control_state_dim))
+              for ele in init_h]
+    init_c = [mx.sym.broadcast_to(mx.sym.expand_dims(mx.sym.Activation(ele, act_type="tanh"),
+                                                     axis=0),
+                                  shape=(batch_size, control_state_dim))
+              for ele in init_c]
+
     # Initialize Control Network
     controller = RNN(num_hidden=[control_state_dim], data_dim=data_dim + num_reads * memory_state_dim,
-                     typ="lstm", name="controller")
+                     typ="lstm",
+                     init_h=init_h,
+                     init_c=init_c,
+                     name="controller")
     mem = NTM(num_reads=num_reads, num_writes=num_writes, memory_size=memory_size,
               memory_state_dim=memory_state_dim, control_state_dim=control_state_dim,
               init_memory=init_memory, init_read_focus=init_read_focus,
               init_write_focus=init_write_focus, name="NTM")
     controller_h = controller.init_h[0]
     controller_c = controller.init_c[0]
-    read_content, read_focus = mem.read(controller_h)
-    erase_signal, add_signal, write_focus = mem.write(controller_h)
+    # read_content, read_focus = mem.read(controller_h)
+    # erase_signal, add_signal, write_focus = mem.write(controller_h)
+    read_content = init_read_content
     controller_states = []
     all_read_focus_l = []
     all_write_focus_l = []
@@ -129,7 +160,18 @@ def sym_gen(seqlen):
                                            num_args=len(all_add_signal_l)),
                              shape=(data_seqlen, -1, num_writes, memory_state_dim)))
                          ])
-
+class NTMInitializer(mx.init.Xavier):
+    def _init_default(self, name, arr):
+        if name == "init_memory":
+            arr[:] = numpy.random.normal(loc=0.1, size=arr.shape)
+        elif name == "init_read_content":
+            arr[:] = numpy.random.normal(loc=0.1, size=arr.shape)
+        elif ("read_head" in name or "write_head" in name) and "init_focus" in name:
+            arr[:] = numpy.broadcast_to(numpy.arange(arr.shape[-1], 0, -1), arr.shape)
+        elif "init_h" in name or "init_c" in name:
+            arr[:] = numpy.random.normal(loc=0.1, size=arr.shape)
+        else:
+            assert False
 vis = PLTVisualizer()
 
 max_input_seq_len = max_length*2 + 2
@@ -137,26 +179,35 @@ max_output_seq_len = max_length
 sym = sym_gen(max_length)
 net = Base(data_shapes={'data': (max_input_seq_len, batch_size, data_dim),
                         'target': (max_output_seq_len, batch_size, data_dim),
-                        'init_memory': (batch_size, memory_size, memory_state_dim),
-                        'NTM->read_head:init_focus': (batch_size, num_reads, memory_size),
-                        'NTM->write_head:init_focus': (batch_size, num_writes, memory_size),
-                        'controller->layer0:init_h': (batch_size, control_state_dim),
-                        'controller->layer0:init_c': (batch_size, control_state_dim)},
+                        'init_memory': (memory_size, memory_state_dim),
+                        'init_read_content': (num_reads, memory_state_dim),
+                        'NTM->read_head:init_focus': (num_reads, memory_size),
+                        'NTM->write_head:init_focus': (num_writes, memory_size),
+                        'controller->layer0:init_h': (control_state_dim,),
+                        'controller->layer0:init_c': (control_state_dim,)},
            sym_gen=sym_gen,
-           default_bucket_kwargs={'seqlen': max_length})
+           learn_init_keys=['init_memory',
+                            'init_read_content',
+                            'NTM->read_head:init_focus',
+                            'NTM->write_head:init_focus',
+                            'controller->layer0:init_h',
+                            'controller->layer0:init_c'],
+           default_bucket_kwargs={'seqlen': max_length},
+           initializer=NTMInitializer(factor_type="in", rnd_type="gaussian", magnitude=2),
+           ctx=mx.gpu())
 net.print_stat()
-init_memory_npy = numpy.tanh(numpy.random.normal(size=(batch_size, memory_size, memory_state_dim)))
+###init_memory_npy = numpy.tanh(numpy.random.normal(size=(batch_size, memory_size, memory_state_dim)))
 # init_memory_npy = numpy.zeros((batch_size, memory_size, memory_state_dim), dtype=numpy.float32) + 0.1
 # init_read_focus_npy = numpy.random.randint(0, memory_size, size=(batch_size, num_reads))
 # init_read_focus_npy = npy_softmax(npy_onehot(init_read_focus_npy, num=memory_size), axis=2)
-init_read_focus_npy = npy_softmax(
-                      numpy.broadcast_to(numpy.arange(memory_size, 0, -1), (batch_size, num_reads, memory_size)),
-                      axis=2)
+###init_read_focus_npy = npy_softmax(
+###                      numpy.broadcast_to(numpy.arange(memory_size, 0, -1), (batch_size, num_reads, memory_size)),
+###                      axis=2)
 # init_write_focus_npy = numpy.random.randint(0, memory_size, size=(batch_size, num_writes))
 # init_write_focus_npy = npy_softmax(npy_onehot(init_write_focus_npy, num=memory_size), axis=2)
-init_write_focus_npy = npy_softmax(
-                       numpy.broadcast_to(numpy.arange(memory_size, 0, -1), (batch_size, num_writes, memory_size)),
-                       axis=2)
+###init_write_focus_npy = npy_softmax(
+###                       numpy.broadcast_to(numpy.arange(memory_size, 0, -1), (batch_size, num_writes, memory_size)),
+###                       axis=2)
 
 # init_read_focus_npy = npy_softmax(numpy.ones((batch_size, memory_size), dtype=numpy.float32), axis=1)
 # init_write_focus_npy = npy_softmax(numpy.ones((batch_size, memory_size), dtype=numpy.float32), axis=1)
@@ -164,8 +215,8 @@ init_write_focus_npy = npy_softmax(
 # init_read_focus_npy = npy_softmax(numpy.random.normal(size=(batch_size, memory_size)), axis=1)
 # init_write_focus_npy = npy_softmax(numpy.random.normal(size=(batch_size, memory_size)), axis=1)
 
-init_h_npy = numpy.zeros((batch_size, control_state_dim), dtype=numpy.float32) + 0.0001#numpy.tanh(numpy.random.normal(size=(batch_size, control_state_dim)))
-init_c_npy = numpy.zeros((batch_size, control_state_dim), dtype=numpy.float32) + 0.0001#numpy.tanh(numpy.random.normal(size=(batch_size, control_state_dim)))
+###init_h_npy = numpy.zeros((batch_size, control_state_dim), dtype=numpy.float32) + 0.0001#numpy.tanh(numpy.random.normal(size=(batch_size, control_state_dim)))
+###init_c_npy = numpy.zeros((batch_size, control_state_dim), dtype=numpy.float32) + 0.0001#numpy.tanh(numpy.random.normal(size=(batch_size, control_state_dim)))
 optimizer = mx.optimizer.create(name='RMSProp', learning_rate=1E-4, rescale_grad=1.0/batch_size)
 updater = mx.optimizer.get_updater(optimizer)
 
@@ -179,12 +230,7 @@ for i in range(max_iter):
         net.forward(is_train=True,
                     bucket_kwargs={'seqlen': seqlen},
                     **{'data': data_in,
-                       'target': data_out,
-                       'init_memory': init_memory_npy,
-                       'NTM->read_head:init_focus': init_read_focus_npy,
-                       'NTM->write_head:init_focus': init_write_focus_npy,
-                       'controller->layer0:init_h': init_h_npy,
-                       'controller->layer0:init_c': init_c_npy})
+                       'target': data_out})
     net.backward()
     norm_clipping(net.params_grad, 10)
     net.update(updater=updater)
@@ -231,12 +277,7 @@ for j in range(2):
             net.forward(is_train=False,
                         bucket_kwargs={'seqlen': seqlen},
                         **{'data': data_in,
-                           'target': data_out,
-                           'init_memory': init_memory_npy,
-                           'NTM->read_head:init_focus': init_read_focus_npy,
-                           'NTM->write_head:init_focus': init_write_focus_npy,
-                           'controller->layer0:init_h': init_h_npy,
-                           'controller->layer0:init_c': init_c_npy})
+                           'target': data_out})
         pred = outputs[0].reshape((seqlen, batch_size, data_dim)).asnumpy()
         state_over_time = outputs[1].asnumpy()
         read_weight_over_time = outputs[2].asnumpy()
@@ -289,12 +330,7 @@ for j in range(2):
             net.forward(is_train=False,
                         bucket_kwargs={'seqlen': seqlen},
                         **{'data': data_in,
-                           'target': data_out,
-                           'init_memory': init_memory_npy,
-                           'NTM->read_head:init_focus': init_read_focus_npy,
-                           'NTM->write_head:init_focus': init_write_focus_npy,
-                           'controller->layer0:init_h': init_h_npy,
-                           'controller->layer0:init_c': init_c_npy})
+                           'target': data_out})
         pred = outputs[0].reshape((seqlen, batch_size, data_dim)).asnumpy()
         state_over_time = outputs[1].asnumpy()
         read_weight_over_time = outputs[2].asnumpy()
