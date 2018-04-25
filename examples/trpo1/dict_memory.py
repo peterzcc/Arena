fixed the bug of kl old not zero

examples/trpo1/dict_memory.py
@@ -64,6 +64,7 @@ class DictMemory(object):
         # self.run_start_times = np.zeros((num_actors,), dtype=np.float32)
 
         self.gpow = self.gamma ** np.arange(0, timestep_limit)
+        self.leaf_info_keys = None
 
     def append_state(self, observation, action, info, pid=0,
                      leaf_id=None, leaf_action=None, leaf_model_info=None, curr_time_step=None):
@@ -76,9 +77,11 @@ class DictMemory(object):
             self.current_path[pid]["root_" + k].append(v)
         if leaf_id is not None:
             self.current_path[pid]["leaf_id"].append(leaf_id)
-            self.current_path[pid]["leaf_action"].append(leaf_action)
+            self.current_path[pid]["leaf_{}_".format(leaf_id) + "action"].append(leaf_action)
+            if self.leaf_info_keys is None:
+                self.leaf_info_keys = leaf_model_info.keys()
             for k, v in leaf_model_info.items():
-                full_key = "leaf_" + k
+                full_key = "leaf_{}_{}".format(leaf_id, k)
                 self.current_path[pid][full_key].append(v)
 
     def append_observation(self, observation, pid=0):
@@ -287,24 +290,36 @@ class DictMemory(object):
         #     path["times"] =
         self.compute_gae_for_path(root_paths, f_critic=self.f_critic)
 
+
         if self.normalize:
             self.normalize_gae(root_paths)
         result = {"paths": root_paths}
         if with_subtasks:
             agg_paths = {}
+            leaf_paths = [dict() for i in range(self.num_leafs)]
             leaf_prefix = "leaf_"
-            for k in paths[0].keys():
+            for k in ["leaf_id"]:
                 if k.startswith(leaf_prefix):
                     agg_paths[k[len(leaf_prefix):]] = np.concatenate([p[k] for p in paths])
+            for pi in range(self.num_leafs):
+                leaf_i_prefix = "leaf_{}_".format(pi)
+                for k in list(self.leaf_info_keys) + ["action"]:
+                    full_k = leaf_i_prefix + k
+                    list_k = [p[full_k] for p in paths if full_k in p]
+                    if len(list_k) != 0:
+                        leaf_paths[pi][k] = np.concatenate(list_k)
+                    else:
+                        leaf_paths[pi][k] = None
             for k in ["observation", "times"]:
                 agg_paths[k] = root_paths[k]
             subrewards = np.concatenate([p["subrewards"] for p in paths])
             agg_paths["reward"] = np.choose(agg_paths["id"], subrewards.T)
+            agg_paths["index"] = np.arange(0, agg_paths["times"].shape[0])
 
             is_leaf_split = np.zeros(root_paths["reward"].shape, dtype=np.bool)
             leaf_term = np.zeros(root_paths["reward"].shape, dtype=np.bool)
 
-            is_leaf_split[root_paths["splits"] - 1] = True
+            is_leaf_split[root_paths["splits"][:-1]] = True
             leaf_term[root_paths["splits"] - 1] = root_paths["terminated"]
 
             leaf_exits = np.flatnonzero(root_paths["action"])
@@ -314,8 +329,7 @@ class DictMemory(object):
             full_leaf_splits = np.append(np.flatnonzero(is_leaf_split), is_leaf_split.shape[0])
             full_leaf_terms = leaf_term[full_leaf_splits - 1]
             full_leaf_ids = agg_paths["id"][full_leaf_splits - 1]
-            del agg_paths["id"]
-
+            # del agg_paths["id"]
             splitted_paths = {}
             for k in agg_paths.keys():
                 if k == "observation":
@@ -325,7 +339,6 @@ class DictMemory(object):
                 else:
                     splitted_paths[k] = np.split(agg_paths[k], full_leaf_splits)
 
-            leaf_paths = [dict() for i in range(self.num_leafs)]
             for l in range(self.num_leafs):
                 this_paths = {}
                 is_this_leaf = full_leaf_ids == l

examples/trpo1/network_models.py
@@ -112,7 +112,8 @@ class MultiNetwork(object):
                                                   trainable=False)
                     time_logit = -self.time_weight * (self.hrl_meta_input[:, 1:2] - self.time_offset)
                     self.fixed_prob_ter_logit = tf.get_variable("fix_ter_prob",
-                                                                initializer=tf.constant(logit(0.01), dtype=tf.float32),
+                                                                initializer=tf.constant(logit(0.01),
+                                                                                        dtype=tf.float32),
                                                                 dtype=tf.float32)
                     cont_prob_offset = 1.0 - self.fixed_prob_ter_logit
                     self.fixed_ter_weight = tf.get_variable("fix_ter_w", initializer=tf.constant(1.0, dtype=tf.float32),
@@ -121,7 +122,7 @@ class MultiNetwork(object):
                                   (1-self.fixed_ter_weight)*time_logit
 
                 full_logits = logits_from_cond_categorical(root_logits, final_contlogit,
-                                                           is_initial_step=self.hrl_meta_input[:, 2:3])
+                                                           is_initial_step=self.hrl_meta_input[:, 2:3])  
                 assert isinstance(self.distribution, Categorical)
                 self.dist_vars, self.old_vars, self.sampled_action, self.interm_vars = \
                     self.distribution.create_dist_vars(logits=full_logits)
@@ -150,12 +151,10 @@ class MultiNetwork(object):
             # Sampled loss of the policy
 
             self.trad_surr_loss = - tf.reduce_mean(self.new_likelihood_sym * self.advant)
+            self.kls = self.distribution.kl_sym(self.old_vars, self.dist_vars)
+            self.kl = tf.reduce_mean(self.kls)
 
-            self.kl = tf.reduce_mean(self.distribution.kl_sym(self.old_vars, self.dist_vars))
             ent_n = self.distribution.entropy(self.dist_vars)  # - self.new_likelihood_sym
             self.ent = tf.reduce_sum(ent_n) / self.batch_size_float
             self.losses = [self.ppo_surr, self.kl, self.ent]
-
-
-
-
+            self.reset_exp = self.distribution.reset_exp(self.interm_vars)

examples/trpo1/policy_gradient_model.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from arena.models.model import ModelWithCritic
 import tensorflow as tf
 from tf_utils import GetFlat, SetFromFlat, flatgrad, var_shape, linesearch, cg, run_batched, concat_feature, \
-    aggregate_feature, select_st, explained_variance_batched, tf_run_batched
+    aggregate_feature, select_st, explained_variance_batched, tf_run_batched, batch_run_forward
 # from baseline import Baseline
 from multi_baseline import MultiBaseline
 from prob_types import DiagonalGaussian, Categorical, CategoricalWithProb
@@ -57,7 +57,8 @@ class PolicyGradientModel(ModelWithCritic):
                  save_model=True,
                  loss_type="PPO",
                  max_grad_norm=0.5,
-                 is_flexible_hrl_model=False
+                 is_flexible_hrl_model=False,
+                 reset_exp=False
                  ):
         ModelWithCritic.__init__(self, observation_space, action_space)
         self.ob_space = observation_space
@@ -285,6 +286,7 @@ class PolicyGradientModel(ModelWithCritic):
         self.full_model_saver = tf.train.Saver(var_list=[*self.critic.var_list, *self.policy.var_list])
         self.has_loaded_model = False
         self.load_old_model = load_old_model
+        self.should_reset_exp = reset_exp
         self.parallel_predict = parallel_predict
         # if self.load_old_model and not self.has_loaded_model:
         #     self.restore_parameters()
@@ -310,6 +312,9 @@ class PolicyGradientModel(ModelWithCritic):
             self.policy_lock.acquire_write()
             if not self.has_loaded_model:
                 self.restore_parameters()
+                if self.should_reset_exp:
+                    logging.info("reset exploration")
+                    self.session.run(self.executer_net.reset_exp)
             self.policy_lock.release_write()
 
         if self.parallel_predict:
@@ -640,7 +645,7 @@ class PolicyGradientModel(ModelWithCritic):
                 #     if hasattr(self.act_space, "low"):
                 #         logging.debug("act_clips: {}".format(np.sum(concat([path["clips"] for path in paths]))))
                 #         # logging.debug("std: {}".format(np.mean(np.exp(np.ravel(action_dist_logstds_n)))))
-                if self.should_update_policy:
+                if self.should_update_policy and batch_size > 0:
                     self.policy_lock.acquire_write()
                     if self.n_fit == 0 and self.is_flexible_hrl_model:
                         self.session.run(tf.assign(self.executer_net.fixed_ter_weight, 0))

examples/trpo1/prob_types.py
@@ -132,8 +132,9 @@ class Categorical(ProbType):
                                          scale=1.0, mode="fan_avg", distribution="normal", dtype=dtype))
         old_dist_vars = dict(logits=old_logits)
         dist_vars = dict(logits=logits)
+        interm_vars = dict(logits=logits)
         sample = tf.distributions.Categorical(logits=logits).sample()
-        return dist_vars, old_dist_vars, sample, {}
+        return dist_vars, old_dist_vars, sample, interm_vars
 
     def log_likelihood_sym(self, x_var, dist_info_vars):
         one_hot_actions = tf.one_hot(x_var, self.n)
@@ -159,6 +160,13 @@ class Categorical(ProbType):
     def kf_loglike(self, action_n, dist_vars, interm_vars):
         return self.log_likelihood_sym(action_n, dist_vars)
 
+    def reset_exp(self, interm_vars, exploration=0.1):
+        return tf.Print(tf.constant(0.0), [], "not implemented")
+        # logits = interm_vars["logits"]
+        # normed_logits = logits - tf.reduce_logsumexp(logits)
+        # final_logits = tf.maximum(normed_logits, np.log(exploration/logits.shape[1].value))
+        # return tf.assign(logits, final_logits)
+
 
 class DiagonalGaussian(ProbType):
     def __init__(self, dim):
@@ -177,20 +185,24 @@ class DiagonalGaussian(ProbType):
                                  kernel_initializer=tf.variance_scaling_initializer(
                                      scale=1.0, mode="fan_avg", distribution="normal", dtype=dtype))
 
-        logstd_param = logstd_1a = tf.get_variable("logstd", (self.dim,), tf.float32,
-                                                   tf.zeros_initializer())  # Variance on outputs
-        logstd_1a = tf.expand_dims(logstd_1a, 0)
+        logstd_param = tf.get_variable("logstd", (self.dim,), tf.float32,
+                                       tf.zeros_initializer())  # Variance on outputs
+        logstd_1a = tf.expand_dims(logstd_param, 0)
         std_1a = tf.exp(logstd_1a)
         logstd_n = tf.tile(logstd_1a, [tf.shape(mean_n)[0], 1])
         std_n = tf.tile(std_1a, [tf.shape(mean_n)[0], 1])
         old_dist_vars = dict(mean=old_mean_n, logstd=old_logstd_n)
         dist_vars = dict(mean=mean_n, logstd=logstd_n)
-        interm_vars = dict(std=std_n)
+        interm_vars = dict(std=std_n, logstd_param=logstd_param)
         sample = tf.distributions.Normal(loc=mean_n, scale=std_n).sample()
         return dist_vars, old_dist_vars, sample, interm_vars
 
     def gen_dist_info(self, mean, log_std):
         return dict(mean=mean, log_std=log_std)
+
+    def reset_exp(self, interm_vars, std=0.05):
+        param = interm_vars["logstd_param"]
+        return tf.assign(param, np.log(np.ones(param.get_shape().as_list())) * std)
 
     def kl_sym(self, old_dist_info_vars, new_dist_info_vars):
         old_means = old_dist_info_vars["mean"]