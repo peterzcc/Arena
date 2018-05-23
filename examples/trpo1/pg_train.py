481a4a6adb8ae58056c6b79d91f4fd0a026680cc
peterzeng <peterzengcc@gmail.com>
2018-05-23 12:30:53 +0800

    hardcode leaf cnn

--- examples/trpo1/pg_train.py
+++ examples/trpo1/pg_train.py
@@ -569,6 +569,7 @@ def main():
                                     ent_k=args.ent_k,
                                     session=session,
                                     load_old_model=args.load_leaf,
+                                    conv_sizes=(((3, 3), 16, 2), ((3, 3), 16, 2), ((3, 3), 4, 2)),
                                     model_load_dir=args.load_dir,
                                     reset_exp=args.reset_exp,
                                     should_train=args.train_leaf,
